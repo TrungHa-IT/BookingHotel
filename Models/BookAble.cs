88d5ecb33c5e4a7a4bef47c92c997c0a124c588d
b1g-nguyx <bichnq88@gmail.com>
2025-02-14 10:52:38 +0700

    update database delete bookingdetail and add a new model bookable

---
+++ Models/BookAble.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBooking.Models
+{
+    public class BookAble
+    {
+        [Key]
+        public int Id { get; set; }
+        public int BookingsId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime? DateTime { get; set; }
+        public int Status { get; set; }
+    }
+}
