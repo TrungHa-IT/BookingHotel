dc742ddcf77e1ba045502c144eb8c47b12de2229
b1g-nguyx <bichnq88@gmail.com>
2025-02-17 11:16:13 +0700

    write updateImage

--- Repositories/ImageRepositories.cs
+++ Repositories/ImageRepositories.cs
@@ -7,13 +7,15 @@ namespace HotelBooking.Repositories
     {
         private readonly UnitOfWork _unitOfWork;
         private readonly ICloudinaryService _icloudinaryService;
-        public ImageRepositories(UnitOfWork unitOfWork,ICloudinaryService cloudinaryService)
+        public ImageRepositories(UnitOfWork unitOfWork, ICloudinaryService cloudinaryService)
         {
             _icloudinaryService = cloudinaryService;
             _unitOfWork = unitOfWork;
         }
         public async Task CreateImageAsync(List<IFormFile> files, int relationID, string relationName)
         {
+
+            List<Image> newImages = new List<Image>();
             int count = 1;
             foreach (var file in files)
             {
@@ -22,16 +24,19 @@ namespace HotelBooking.Repositories
                     RID = relationID,
                     Relation = relationName,
                     imageURL = await _icloudinaryService.UploadImageAsync(file),
-                    name = relationName +"~"+relationID+"~"+ count+".jpg",
+                    name = relationName + "~" + relationID + "~" + count + ".jpg",
                 };
-                _unitOfWork._imageRepository.Add(image);
-                await _unitOfWork.SaveChangesAsync();
+                newImages.Add(image);
+                count++;
             }
+            _unitOfWork._imageRepository.AddRange(newImages);
+            await _unitOfWork.SaveChangesAsync();
+
         }
 
         public async Task DeleteImageAsync(int id)
         {
-           var image = await _unitOfWork._imageRepository.GetByIdAsync(id);
+            var image = await _unitOfWork._imageRepository.GetByIdAsync(id);
             if (image != null)
             {
                 _unitOfWork._imageRepository.Remove(image);
@@ -42,17 +47,47 @@ namespace HotelBooking.Repositories
         public async Task<Image> GetImageAsync(int id)
         {
 
-           return await _unitOfWork._imageRepository.GetByIdAsync(id);
+            return await _unitOfWork._imageRepository.GetByIdAsync(id);
         }
 
         public async Task<IEnumerable<Image>> GetAllImagesAsync()
         {
-           return await _unitOfWork._imageRepository.GetAllAsync();
+            return await _unitOfWork._imageRepository.GetAllAsync();
         }
 
         public async Task<List<Image?>> GetAllImageByIdOfRelationID(int relationID, string relationName)
         {
             return await _unitOfWork._imageRepository.GetImageByRIDAsync(relationID, relationName);
+        }
+
+        public async Task UpdateAllImage(List<IFormFile> files, string relationName, int relationID)
+        {
+            List<Image?> images = await _unitOfWork._imageRepository.GetImageByRIDAsync(relationID, relationName);
+
+            if (images != null && images.Count > 0)
+            {
+                _unitOfWork._imageRepository.RemoveRange(images);
+                await _unitOfWork.SaveChangesAsync();
+            }
+
+            List<Image> newImages = new List<Image>();
+            int count = 1;
+            foreach (var file in files)
+            {
+                var image = new Image
+                {
+                    RID = relationID,
+                    Relation = relationName,
+                    imageURL = await _icloudinaryService.UploadImageAsync(file),
+                    name = relationName + "~" + relationID + "~" + count + ".jpg",
+                };
+                newImages.Add(image);
+                count++;
+            }
+
+            _unitOfWork._imageRepository.AddRange(newImages);
+
+            await _unitOfWork.SaveChangesAsync();
         }
     }
 }
